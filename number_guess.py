17161d9b2baa55b00adb50ed89aae75c00eb14f5
sky2497 <praveenhamburg2497@gmail.com>
2023-01-19 13:33:21 +0100

    frist commit

---
+++ number_guess.py
@@ -0,0 +1,21 @@
+import random
+
+top_of_range = input("type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+
+    if top_of_range <= 0:
+        print('Please type a number larger than 0 next time.')
+        quit()
+
+else:
+     print("Please type a number next time.")
+     quit()
+
+       
+
+random_number = random.randrange(-5 , 11)
+
+print(random_number)
